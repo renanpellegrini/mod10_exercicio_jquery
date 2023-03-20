ecebe61254fe8d48e2091c2d8a92dfe66397fc78
Renan <renanpellegrini@outlook.com>
2023-03-20 20:35:13 -0300

    commit inicial

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+    
+    $('#telefone').mask('(00) 00000-0000')
+    $('#cpf').mask('000.000.000-00')
+    $('#CEP').mask('00.000-000')
+
+
+    $('form').validate({
+        rules:{
+            nomeCompleto: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            cpf: {
+                required: true
+            },
+            telefone: {
+                required: true
+            },
+            enderecoCompleto: {
+                required: true
+            },
+        },
+        messages: {
+            nomeCompleto: 'Por favor, insira seu nome',
+            telefone: 'Por favor, insira seu telefone',
+            email: 'Por favor, insira seu e-mail',
+            enderecoCompleto: 'Por favor, insira seu endereço',
+            cpf: 'Por favor, insira seu CPF',
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if(camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+    })
